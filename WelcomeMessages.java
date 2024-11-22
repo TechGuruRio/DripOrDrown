1f43b25c41a6cd38ce9a663e083984d4ef916aff
TechGuruRio <ToClean87@gmail.com>
2024-11-22 16:18:56 -0500

    Add files via upload

---
+++ WelcomeMessages.java
@@ -0,0 +1,6 @@
+public class WelcomeMessages {public static void main(String[] args) {
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Computer Science");
+    System.out.println("Programming is fun");
+} 
+}

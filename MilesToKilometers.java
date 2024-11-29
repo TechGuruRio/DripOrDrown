b684d87707ffe8a04ee8b8326ce0f7c1725262fb
TechGuruRio <ToClean87@gmail.com>
2024-11-29 17:25:24 -0500

    Add files via upload

---
+++ MilesToKilometers.java
@@ -0,0 +1,9 @@
+public class MilesToKilometers {
+    public static void main(String[] args) {
+        System.out.println("Miles\t\tKilometers");
+        for (int miles = 1; miles <= 10; miles++) {
+            double kilometers = miles * 1.609;
+            System.out.printf("%-10d\t%.3f%n", miles, kilometers);
+        }
+    }
+}
